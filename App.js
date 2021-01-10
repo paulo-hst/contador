ef582674906a9cb1b02c8ba4be0f3f6a6e5ab81e
Paulo Teixeira <hst.paulo@gmail.com>
2021-01-10 16:53:36 -0300

    counter and reset buttons

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [ count, setCount ] = useState(0)
+
+  // Função - exemplo 1
+  const counter = () => {
+    setCount(count + 1)
+  }
+
+  // Função - exemplo 2
+  function reset(){
+    setCount(0)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Contador!</Text>
+      <Text style={styles.titleElement}>Contador</Text>
+
+      <TouchableOpacity style={styles.buttonElement} onPress={ counter }>
+        <Text style={styles.buttonTextElement}>Clique Aqui!</Text>  
+      </TouchableOpacity>
+
+      <Text style={styles.textElement}>Valor: {count} </Text>
+      
+      <TouchableOpacity style={styles.resetElement} onPress={ reset }>
+        <Text style={styles.buttonTextElement}>Resetar</Text>  
+      </TouchableOpacity>
+ 
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +38,51 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  titleElement: {
+    fontSize: 80,
+    top: -40,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+
+  buttonElement: {
+    width: '50%',
+    backgroundColor: '#00BFFF',
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 30,
+
+    justifyContent: 'center',
+  },
+
+  buttonTextElement: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+  },
+
+  textElement: {
+    fontSize: 40,
+    marginTop: 30,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  
+
+  resetElement: {
+    width: '50%',
+    backgroundColor: 'red',
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 30,
+
+    justifyContent: 'center',
+  },
+
 });
